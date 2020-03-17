3fa0f0dc39cbd7f44b5222ef1594e410eeaeb7bc
Ismail Kombe <ismailkombe@gmail.com>
2020-03-17 22:45:51 +0100

    Corona

--- src/main/java/com/ikoembe/model/nedim.java
+++ src/main/java/com/ikoembe/model/nedim.java
@@ -4,4 +4,7 @@ public class nedim {
     public static void main(String[] args) {
         System.out.println(" life is cool");
     }
+    public void coronaVirus(){
+        System.out.println("Corona 2019");
+    }
 }
