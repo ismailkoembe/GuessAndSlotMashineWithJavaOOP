befc695240167cee535e690af8b932e96bf5237f
Burhan PEKTAS <pektasburhan39@gmail.com>
2023-10-23 23:24:54 +0200

    burhan branch ikinci degisiklik

---
+++ src/main/java/funktion2.java
@@ -0,0 +1,6 @@
+public class funktion2 {
+    public static void main(String[] args) {
+        System.out.println("burhan branch'inde calisiyorum");
+        System.out.println("burhan branchinde ikinci degisiklik");
+    }
+}
