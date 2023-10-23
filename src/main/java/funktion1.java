dd0f33fe5776d72a702a32433a6b739d0fb37946
Burhan PEKTAS <pektasburhan39@gmail.com>
2023-10-23 23:28:16 +0200

    main branch degisiklik

---
+++ src/main/java/funktion1.java
@@ -0,0 +1,8 @@
+public class funktion1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println("main branch degisiklik 1.satir");
+        System.out.println("main branch degisiklik 2.satir");
+    }
+}
