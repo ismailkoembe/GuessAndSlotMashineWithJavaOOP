a449f81c8bf358d471212ce81e6c0e605ca2cd5f
Burhan PEKTAS <pektasburhan39@gmail.com>
2023-10-24 00:01:24 +0200

    funktion 1

--- src/main/java/funktion1.java
+++ src/main/java/funktion1.java
@@ -2,5 +2,7 @@ public class funktion1 {
     public static void main(String[] args) {
         System.out.println("Burhan branch'inde calisiyorum");
 
+        System.out.println("Artik Github ögrenelim");
+
     }
 }
