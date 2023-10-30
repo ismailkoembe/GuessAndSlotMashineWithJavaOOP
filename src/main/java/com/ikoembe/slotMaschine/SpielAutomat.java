ae8821cb5a11e5ae0cf71c6590e803572b7f5290
aslierk <asliekmekc@gmail.com>
2023-10-30 22:32:34 +0100

    Asli's second commit

--- src/main/java/com/ikoembe/slotMaschine/SpielAutomat.java
+++ src/main/java/com/ikoembe/slotMaschine/SpielAutomat.java
@@ -26,8 +26,7 @@ import java.util.Random;
     public static boolean run(boolean flag){
 
         while(flag) {
-            System.out.println("Geben Sie den Geldbetrag ein, den Sie in "
-                        + "den Spielautomaten stecken möchten.");
+            System.out.println("Enter the amount of money you want to put into the slot machine.");
             
             amount=Reader.readDouble();  
             tAmount+=amount;        
@@ -47,7 +46,7 @@ import java.util.Random;
     public static double playTheGame(double amountMethod, double tWinMethod) {
         CashBox user = new CashBox();
      
-    String [] obst={"Kirschen", "Orangen", "Pflaumen","Glocken","Melonen","Birne"};
+    String [] fruits={"Cherries", "Oranges", "Plums", "Bells", "Melons", "Pear"};
         
     String [] nummer= new String[3];
         
@@ -58,7 +57,7 @@ import java.util.Random;
             
             random = rn.nextInt(5);
             
-            nummer[i]=obst[random];
+            nummer[i]=fruits[random];
             
             System.out.print(nummer[i]+"  ");   
         }
@@ -66,18 +65,18 @@ import java.util.Random;
         System.out.println();
 
         if (nummer[0].equals(nummer[1]) && nummer[0].equals(nummer[2])) {
-            System.out.println("**************Glückwunsch************"+"/nYou won "
+            System.out.println("**************************"+"/nYou won "
              + (3*amountMethod)+ " ");
             tWinMethod+=3*amountMethod;
             user.profitCalc(tWinMethod);
          
         }else if (nummer[0].equals(nummer[1]) || nummer[0].equals(nummer[2]) || nummer[1].equals(nummer[2])) {
-            System.out.println("**************Glückwunsch************"+"/nYou won  "+ (2*amountMethod)+"€");
+            System.out.println("**************Congratulations************"+"/nYou won  "+ (2*amountMethod)+"€");
             tWinMethod+=2*amountMethod;
             user.profitCalc(tWinMethod);
    
         }else {
-            System.out.println("Oooops! :( Sie verdienen kein Geld.");
+            System.out.println("Oooops! :( You do not earn money.");
             user.profitCalc(-amountMethod);
         }
         
