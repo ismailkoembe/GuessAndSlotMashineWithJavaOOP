07245ea4b213aa423267b8f2cf46b24611672527
reyhany <breyhanyildiz@gmail.com>
2023-11-06 08:42:44 +0100

    v2.4

--- src/main/java/Testing_GE.java
+++ src/main/java/Testing_GE.java
@@ -2,5 +2,6 @@ public class Testing_GE {
     public static void main(String[] args) {
         System.out.println("Deneme by Gamze Erdogan");
         System.out.println("Ikinci deneme");
+        System.out.println("Ucuncu deneme");
     }
 }
